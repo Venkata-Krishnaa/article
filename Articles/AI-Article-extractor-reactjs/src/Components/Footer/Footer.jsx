f1347713c1ae4f558818e332c42fa15dd950ab74
VENKATA KRISHNA M <167985988+Venkata-Krishnaa@users.noreply.github.com>
2024-12-03 12:26:11 +0530

    Add files via upload

---
+++ Articles/AI-Article-extractor-reactjs/src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        Developed with 💖 by : VEnkata Krishna Rao
+      </footer>
+    </>
+  )
+}
+
+export default Footer
